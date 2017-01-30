70f96ce8463d50b9495225100ada3ca7f9ca34fe
King James <King James>
2017-01-30 11:59:59 -0500

    tested header.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(require('./middleware/headers'));
+
 app.listen(3000, function(){
 	console.log("app is listening on port 3000");
 });
