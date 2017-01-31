85c5532a12966da082ac078b08d412c6cd604c22
King James <King James>
2017-01-31 14:31:46 -0500

    completed module 14 with signups

---
+++ client/user/auth.js
@@ -0,0 +1,47 @@
+$(function(){
+	$.extend(WorkoutLog, {
+		//signup method
+		signup: function(){
+			//username & password variables.
+			var username = $('#su_username').val();
+			var password = $('#su_password').val();
+			//user object
+			var user = {
+				user: {
+					username: username,
+					password: password
+				}
+			};
+
+			//signup post
+			var signup = $.ajax({
+				type: "POST",
+				url: WorkoutLog.API_BASE + "user",
+				data: JSON.stringify(user),
+				contentType: "application/json"
+			});
+
+			//signup done/fail
+			signup.done(function(data) {
+				if(data.sessionToken) {
+					WorkoutLog.setAuthHeader(data.sessionToken);
+				}
+
+				$("#signup-modal").modal("hide");
+				$(".disabled").removeClass("disabled");
+				$("#loginout").text("Logout");
+					}).fail(function() {
+				$("#su_error").text("There was an issue with sign up").show();
+				});
+
+		}
+
+		//login method
+
+		//loginout method
+	});
+
+	//bind events
+	$("#signup").on("click", WorkoutLog.signup);
+
+});
