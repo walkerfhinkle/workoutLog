fc4f331041fd8a95fa34e615590049298ae76fc1
King James <King James>
2017-02-01 14:53:13 -0500

    completed module 19 with server logs

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.use('/api/user', require('./routes/user'));
 //login route
 app.use('/api/login', require('./routes/session'));
 app.use('/api/definition', require('./routes/definition'));
+app.use('/api/log', require('./routes/log'));
 
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
---
+++ server/models/log.js
@@ -0,0 +1,10 @@
+//build a model in sequelize
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('log', {
+		description: DataTypes.STRING,
+		result: DataTypes.STRING,
+		owner: DataTypes.INTEGER,
+		def: DataTypes.STRING
+	},{
+	});
+};
